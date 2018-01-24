49d8d8a2c852d288d3f22d3649c12641cc982247
Brandon O. Niles <b21niles@gmail.com>
2018-01-24 16:57:31 -0500

    putting together interface for UX. added display

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Display from './Display.js';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    displayValue: 0,
+    firstNum: null
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">QuickSnax</h1>
+          <p><i>If we don't have it, you don't need it.</i></p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Display inputValue={this.state.displayValue} />
       </div>
     );
   }
---
+++ src/Display.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './Display.css'
+
+export default class Display extends Component {
+  render () {
+    return (
+      <input id="display" className="display" value={this.props.inputValue} readOnly/>
+    )
+  }
+}
